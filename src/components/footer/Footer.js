f11e6758e6e4a4013eaca6498c6e76c0b04a95c2
Cedrick Monesit <cedrickmonesit@gmail.com>
2020-03-25 16:55:48 -0400

    Added Footer
    Added a footer with contact information and TMDB logo

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { FaFacebookSquare, FaInstagram, FaGithubSquare } from "react-icons/fa";
 
 import "./footer.scss";
 import TMDB from "../../images/TMDB.png";
@@ -9,10 +11,18 @@ const Footer = () => {
       <section className="main-footer-top">
         <header className="main-footer-top-header">
           <h2>Film Flix</h2>
-          <nav>Nav</nav>
+          <nav className="main-footer-nav">
+            <Link to="/">Home </Link>
+            <Link to="/">Profile </Link>
+            <Link to="/">Favorites </Link>
+          </nav>
         </header>
-        <div className="main-footer-item">portfolio: cedrickmonesit.xyz</div>
-        <div className="main-footer-item">phone number: 508-494-4306</div>
+        <p className="main-footer-item">
+          portfolio: <a href="/">cedrickmonesit.xyz</a>
+        </p>
+        <p className="main-footer-item">
+          phone number: <a href="tel: 508-494-4306">508-494-4306</a>
+        </p>
         <p className="main-footer-top-copyright">
           Copyright @2020 <br />
           Code and design by
@@ -27,7 +37,7 @@ const Footer = () => {
         </p>
       </section>
       <section className="main-footer-bottom">
-        <div>
+        <div className="main-footer-bottom-logo">
           <a
             href="https://www.themoviedb.org/"
             alt="TMDB anchor tag"
@@ -37,7 +47,29 @@ const Footer = () => {
             <img className="main-footer-tmdb-logo" src={TMDB} alt="TMDB Logo" />
           </a>
         </div>
-        <div>Social Media</div>
+        <div className="main-footer-bottom-social-icons">
+          <a
+            href="https://www.facebook.com/princejohn.monesit"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            <FaFacebookSquare className="main-footer-social-icon" />
+          </a>
+          <a
+            href="https://www.instagram.com/cedrickmonesit/"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            <FaInstagram className="main-footer-social-icon" />
+          </a>
+          <a
+            href="https://github.com/cedrickmonesit"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            <FaGithubSquare className="main-footer-social-icon" />
+          </a>
+        </div>
       </section>
     </div>
   );
