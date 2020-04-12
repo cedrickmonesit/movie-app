2e9f4527f59937ce3edf34ed9bea83777248d019
Cedrick Monesit <cedrickmonesit@gmail.com>
2020-04-12 18:38:39 -0400

    deploying site

---
+++ build/precache-manifest.27be87a70072eae97cf5de395a659298.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e1b564c8d223397b376a120643c0db0",
+    "url": "/film-flix/index.html"
+  },
+  {
+    "revision": "f2da4b00f35600c7ca3b",
+    "url": "/film-flix/static/css/2.ca900277.chunk.css"
+  },
+  {
+    "revision": "65df071df25202d2dd4e",
+    "url": "/film-flix/static/css/main.fe39636c.chunk.css"
+  },
+  {
+    "revision": "f2da4b00f35600c7ca3b",
+    "url": "/film-flix/static/js/2.d3b2c90c.chunk.js"
+  },
+  {
+    "revision": "7d214800dd318343323fda2f2d085839",
+    "url": "/film-flix/static/js/2.d3b2c90c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65df071df25202d2dd4e",
+    "url": "/film-flix/static/js/main.6bc9c7db.chunk.js"
+  },
+  {
+    "revision": "69b55510c3e8c7224da4",
+    "url": "/film-flix/static/js/runtime-main.43715149.js"
+  },
+  {
+    "revision": "5f886995247a33ada9e8efebdbc1f165",
+    "url": "/film-flix/static/media/cinema.5f886995.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/film-flix/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/film-flix/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/film-flix/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/film-flix/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "55c39d0ce0a7103f0655ec035e07a095",
+    "url": "/film-flix/static/media/word-logo.55c39d0c.png"
+  }
+]);
